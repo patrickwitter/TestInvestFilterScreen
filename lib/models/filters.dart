fb4f983c11762d6f67b93135d628ee8d50e70ada
Patrick Witter <pwitterop123@gmail.com>
2022-05-21 20:13:28 -0500

    Added filters model

---
+++ lib/models/filters.dart
@@ -0,0 +1,7 @@
+class Filters {
+  static List<String> popularity = ["A", "B", "C", "D"];
+  static List<String> currency = ["A", "B", "C", "D"];
+  static List<String> name = ["A", "B", "C", "D"];
+  static List<String> symbol = ["A", "B", "C", "D", "C", "D"];
+  static List<String> price = ["no minimum", "A", "B", "C", "D"];
+}
